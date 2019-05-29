32ea04c547b4e47327041ce93fe00dd47b09840d
Manish Dua <manishdua@Manishs-MacBook-Pro.local>
2019-05-29 13:42:22 -0400

    newfile

--- index29.js
+++ index29.js
@@ -1,8 +1,1 @@
 const sqlite3 = require("sqlite3").verbose();
-
-
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin git@github.com:alexpchin/<reponame>.git
-git push -u origin master
